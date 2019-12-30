8772cafd21907fd996a503bf693d9bd1ca8ead9d
JustAsk4 <59086302+JustAsk4@users.noreply.github.com>
2019-12-30 17:28:18 +0200

    Rename old single file version
    Rename old single file version - not to be used anymore.

--- xx-old-single-file gravity shoot.py
+++ xx-old-single-file gravity shoot.py
@@ -23,7 +23,7 @@ BLUE = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Gravity Shoot (JK2017)")
+pygame.display.set_caption("Gravity Shoot (JK2019)")
 clock = pygame.time.Clock()
 
 
